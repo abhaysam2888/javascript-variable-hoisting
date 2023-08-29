b7fd43601161d7db1a487328c7ebd28a8e33f1cf
abhaysam2888 <abhaysam2888@gmail.com>
2023-08-29 14:51:39 +0530

    all file added

---
+++ script.js
@@ -0,0 +1,44 @@
+// question 1
+
+addNumbers(1, 2)
+
+function addNumbers(num, num2) {
+    console.log( num + num2 );
+}
+
+// quetsion 2
+
+// console.log(experssion(2,3));
+const experssion = function multiplyNumbers(num, num2) {
+    console.log(num * num2);
+}
+
+// question 3
+function calsculateSum(num, num2) {
+    console.log(sum);
+    var sum = num + num2
+}
+
+calsculateSum(7, 3)
+
+// question 4
+
+{
+    console.log(a);
+    let a = 10
+    console.log(a);
+
+    console.log(b);
+    var b = 20
+    console.log(b);
+
+    console.log(c);
+    const c = 30
+    console.log(c);
+}
+
+// quetsion 5
+{
+    console.log(b);
+    let b = 50
+}
